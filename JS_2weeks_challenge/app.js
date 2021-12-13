119b2d438d2d5494027d4374dbef610789931467
Ubermensch0608 <melancholyee28@gmail.com>
2021-12-14 00:57:11 +0900

    JS-chanllenge-로그인시 input창 숨김 구현

--- JS_2weeks_challenge/app.js
+++ JS_2weeks_challenge/app.js
@@ -1,4 +1,15 @@
 function newNameChecker() {
   const newName = document.getElementById("nameBox").value;
   document.querySelector(".greeting").innerHTML = "Hello, " + newName;
+
+  const inputBox = document.getElementById("nameBox");
+  const newAttr = document.createAttribute("class");
+  inputBox.setAttributeNode(newAttr);
+
+  if (newAttr.value === "") {
+    newAttr.value = "make-hide";
+  } else if (newAttr.value === "make-hide") {
+    newAttr.value = "";
+    document.querySelector("button").innerText = "check out!";
+  }
 }
